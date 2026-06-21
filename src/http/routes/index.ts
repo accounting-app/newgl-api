beb25d5f8cafb544fb92097695f6df864a22cac1
Hector Garcia <hmgarcia06@gmail.com>
2026-06-21 12:44:45 -0400

    Created routes/index.ts for cleaner route exports

---
+++ src/http/routes/index.ts
@@ -0,0 +1,9 @@
+import { accountRoutes } from "@/http/routes/accounts";
+import { ledgerRoutes } from "@/http/routes/ledger";
+import { transactionRoutes } from "@/http/routes/transactions";
+
+export {
+    accountRoutes,
+    ledgerRoutes,
+    transactionRoutes
+}
