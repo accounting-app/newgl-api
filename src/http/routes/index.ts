4f0ea2d7e460aaf452125b538bd0eec3009f5a72
Hector Garcia <hmgarcia06@gmail.com>
2026-06-21 13:36:11 -0400

    Implemented health and openapi to http/routes/index.ts

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { accountRoutes } from "@/http/routes/accounts";
 import { ledgerRoutes } from "@/http/routes/ledger";
+import { openApiRoutes } from "@/http/routes/openapi";
 import { transactionRoutes } from "@/http/routes/transactions";
+import { healthRoutes } from "@/http/routes/health";
 
 export {
     accountRoutes,
     ledgerRoutes,
-    transactionRoutes
+    openApiRoutes,
+    transactionRoutes,
+    healthRoutes
 }
